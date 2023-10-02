559e0b7cdd8be58a8cdd0fca482ea4342e2fa6d5
bezites <bezites18@gmail.com>
2023-10-02 06:01:19 +0100

    my second commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let array1 = [e, g, i, k]
+let array2 = [f, h, j, l]
+let newArray = [a,b,c,d]
+
+ function prod(){
+    let a = e/f
+    let b = g/h
+    let c = i/j 
+    let d = k/l;
+
+
+    let e = 15
+    let f = 5
+    let g = 30
+    let h = 6
+    let i = 45
+    let j = 3
+    let k = 60
+    let l = 10
+
+    
+
+let result = (a, b, c, d)
+return prod()
+}
+ console.log(prod(a,b,c,d));
+ console.log(newArray);
